21a0b1a13ec4398972e35f443a55f8c2f491c7a2
Hexcles Ma <bob1211@gmail.com>
2013-04-02 16:56:40 +0800

    fixes

--- setup.sh
+++ setup.sh
@@ -3,7 +3,4 @@
 ln -sf ./home/vimrc ~/.vimrc
 ln -sf ./home/gvimrc ~/.gvimrc
 
-sudo cp -f ./etc/xprofile /etc/xprofile
-sudo cp -f ./etc/Xresources /etc/Xresources
-sudo cp -f ./etc/fonts.conf /etc/fonts/fonts.conf
-sudo cp -f ./etc/vimrc /etc/vimrc
+sudo cp -rf ./etc/ /etc/
