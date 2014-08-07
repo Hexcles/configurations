78a3ac1525a24c654f9d7d71dcc2f15d7c3ce173
Hexcles Ma <bob1211@gmail.com>
2014-08-07 10:55:19 +0800

    add profile

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ln -sf `pwd`/home/profile ~/.profile
 ln -sf `pwd`/home/vimrc ~/.vimrc
 ln -sf `pwd`/home/gvimrc ~/.gvimrc
 ln -sf `pwd`/home/ycm_extra_conf_default.py ~/.vim/bundle/YouCompleteMe/
