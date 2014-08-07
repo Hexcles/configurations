50f59b551b4edf4c7ac04cb142ba246cc4d8bc3e
Hexcles Ma <bob1211@gmail.com>
2014-08-07 17:08:30 +0800

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,10 @@
 find . -name "*~" -delete
 
 for dotfile in `pwd`/home/*; do
+	echo Installing `basename $dotfile`
 	ln -sf $dotfile ~/.`basename $dotfile`
 done
+
 ln -sf `pwd`/other/ycm_extra_conf_default.py ~/.vim/bundle/YouCompleteMe/
 
-sudo cp -rf etc/ /etc/
+sudo cp -rfv etc/ /
