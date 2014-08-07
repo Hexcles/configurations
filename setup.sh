0bf6ea1b7bbfb08aa10c2c0ed2f8df6aa46c63fa
Hexcles Ma <bob1211@gmail.com>
2014-08-07 15:49:16 +0800

    Move some configurations from profile to xprofile

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ln -sf `pwd`/home/profile ~/.profile
+ln -sf `pwd`/home/xprofile ~/.xprofile
 ln -sf `pwd`/home/vimrc ~/.vimrc
 ln -sf `pwd`/home/gvimrc ~/.gvimrc
 ln -sf `pwd`/home/ycm_extra_conf_default.py ~/.vim/bundle/YouCompleteMe/
 
+find etc/ -name "*~" -delete
 sudo cp -rf etc/ /etc/
