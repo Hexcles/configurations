f2dd133a21e2a848153a1d4d69b364c46a2f977e
Hexcles Ma <bob1211@gmail.com>
2015-10-26 11:12:04 -0700

    Tidy up aliases

---
+++ home/aliases.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#alias goagent='(cd ~/bin/goagent && nohup python2 proxy.py 2> /dev/null &)'
+#alias shadowsocks='nohup sslocal -c ~/bin/shadowsocks.json >/dev/null 2>&1 &'
+#alias setproxy='export http_proxy="http://127.0.0.1:3128" && export https_proxy="https://127.0.0.1:3128"'
+
+# Colorful commands
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
+alias ll='ls -l'
+
+# Handy shortcuts
+alias remapkey='setxkbmap && xmodmap ~/.Xmodmap'
+alias open='xdg-open'
+alias douban.fm='python2 ~/studio/douban.fm/douban/douban.py'
+alias canvas_decode='~/bin/canvas_urldecode.py'
