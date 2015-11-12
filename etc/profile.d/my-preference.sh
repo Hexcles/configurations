63db250024146950edcd917e075fd0d388ad208f
Hexcles Ma <bob1211@gmail.com>
2015-11-11 16:03:38 -0800

    Finally settle down LANG envs

--- etc/profile.d/my-preference.sh
+++ etc/profile.d/my-preference.sh
@@ -1,5 +1,9 @@
 # /etc/profile.d/my-preference.conf
 
 # Use Vim!
-EDITOR="vim"
-export EDITOR
+export EDITOR=vim
+
+# Display messages in English when we're not running desktop.
+if [ -z $DESKTOP_SESSION ]; then
+	export LC_MESSAGES=en_US.UTF-8
+fi
