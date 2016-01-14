07563bf170fac5a2f03eb6f9c11d580fd61905bd
Hexcles Ma <bob1211@gmail.com>
2016-01-13 16:53:10 -0800

    Adding shadowsocks back

--- home/aliases.sh
+++ home/aliases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #alias goagent='(cd ~/bin/goagent && nohup python2 proxy.py 2> /dev/null &)'
-#alias shadowsocks='nohup sslocal -c ~/bin/shadowsocks.json >/dev/null 2>&1 &'
+alias shadowsocks='nohup ss-local -c ~/bin/shadowsocks.json -A --fast-open >/dev/null 2>&1 &'
 #alias setproxy='export http_proxy="http://127.0.0.1:3128" && export https_proxy="https://127.0.0.1:3128"'
 
 # Colorful commands
@@ -15,3 +15,5 @@ alias remapkey='~/.xkb/mapkey.sh'
 alias open='xdg-open'
 alias douban.fm='python2 ~/studio/douban.fm/douban/douban.py'
 alias canvas_decode='~/bin/canvas_urldecode.py'
+
+alias matlab='LANG=en_US.UTF-8 /opt/MATLAB/bin/matlab 2>/dev/null ; rm -f ~/java.log.*'
